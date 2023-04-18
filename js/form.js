726d7d016ec0d126cb7a88bd3f1bbd0ca7086032
fantasiaproducciones <129820812+fantasiaproducciones@users.noreply.github.com>
2023-04-18 09:57:52 -0500

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,40 @@
+document.querySelector("#submit").addEventListener("click", e => {
+  e.preventDefault();
+
+  //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+  const telefono = "573136283643";
+
+  const cliente = document.querySelector("#cliente").value;
+  const telefono = document.querySelector("#telefono").value;
+  const mensaje = document.querySelector("#mensaje").value;
+  
+  const resp = document.querySelector("#respuesta");
+
+  resp.classList.remove("fail");
+  resp.classList.remove("send");
+
+  const url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+		*_MI NEGOCIO_*%0A
+		*Reservas*%0A%0A
+		*¿Cuál es tu nombre?*%0A
+		${cliente}%0A
+		*Indica la fecha de tu reserva*%0A
+		${fecha}%0A
+		*Indica la hora de tu reserva*%0A
+		${hora}%0A
+		*Empleado de preferencia*%0A
+		${empleado}%0A
+		*¿Cuál es el servicio que se desea realizar?*%0A
+		${servicio}`;
+
+  if (cliente === "" || fecha === "" || hora === "") {
+    resp.classList.add("fail");
+    resp.innerHTML = `Faltan algunos datos, ${cliente}`;
+    return false;
+  }
+  resp.classList.remove("fail");
+  resp.classList.add("send");
+  resp.innerHTML = `Se ha enviado tu reserva, ${cliente}`;
+
+  window.open(url);
+});
